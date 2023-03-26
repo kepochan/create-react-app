8097c3b11f7a2b8e700d8c3a51165b736551f5c0
kepochan <brousmiche3@gmail.com>
2023-03-26 16:59:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ function App() {
         <p>
           
 <iframe
-    src="https://ipfs.thirdwebcdn.com/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/erc1155.html?contract=0x939a7C3FDa542fF9E515ec380970D7043aA02c58&chain=%7B%22name%22%3A%22Mumbai%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fmumbai.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22maticmum%22%2C%22chainId%22%3A80001%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22mumbai%22%7D&tokenId=0&biconomyApiKey=OUotQOSlx.9f1d22b6-5c15-48f4-910f-3e721471a3d9&biconomyApiId=b2530420-1026-4807-9c5c-296f0e61bf9d"
+    src="https://ipfs.thirdwebcdn.com/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/erc1155.html?contract=0x939a7C3FDa542fF9E515ec380970D7043aA02c58&chain=%7B%22name%22%3A%22Mumbai%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fmumbai.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22maticmum%22%2C%22chainId%22%3A80001%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22mumbai%22%7D&tokenId=1&biconomyApiKey=OUotQOSlx.9f1d22b6-5c15-48f4-910f-3e721471a3d9&biconomyApiId=b2530420-1026-4807-9c5c-296f0e61bf9d%09&primaryColor=blue"
     width="600px"
     height="600px"
+    style="max-width:100%;"
     frameborder="0"
     ></iframe>
         </p>
